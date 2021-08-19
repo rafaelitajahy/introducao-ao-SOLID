d7a91b7b63b8ac40b6627d4e4ca9efdf09b6068a
rafaelitajahy <rafaelitajahy@gmail.com>
2021-08-18 22:03:26 -0300

    test complete

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,47 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+    });
+
+    this.users.push(user);
+
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    const user = this.users.find((user) => user.id === id);
+
+    return user;
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    const user = this.users.find((user) => user.email === email);
+
+    return user;
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const { id } = receivedUser;
+
+    const userIndex = this.users.findIndex((user) => user.id === id);
+
+    const updateUser = {
+      ...this.users[userIndex],
+      ...{ admin: true, updated_at: new Date() },
+    };
+
+    this.users[userIndex] = updateUser;
+
+    return updateUser;
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
